f10b1b4ce5c1bf446cf9f785b2ea921166090796
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2024-06-07 09:13:42 +0300

    Created Middleware functions

---
+++ middleware/verifySignup.js
@@ -0,0 +1,60 @@
+const db  = require("../models");
+const ROLES = db.ROLES;
+const User = db.User
+
+const checkDuplicateUsernameOrEmail = (req, res, next) => {
+    //Username
+    User.findOne({
+        where: {
+            username: req.query.username
+        }
+    })
+    .then(user => {
+        if(user) {
+            res.status(403).send({
+                message: "Failed! Username is already in use!"
+            })
+        }
+        return ;
+    })
+
+    //Email
+    User.findOne({
+        where: {
+            email: req.query.email
+        }
+    })
+    .then(user => {
+        if(user){
+            res.status(403).send({
+                message: "Failed! Email is already in use!"
+            })
+        return ;
+        }
+
+    })
+
+    next();
+    
+}
+
+const checkRolesExisted = (req, res, next) => {
+    if(req.query.roles) {
+        for (let i = 0; i < req.query.roles.length; i++) {
+            if(!ROLES.includes(req.query.roles[i])){
+                res.status(400).send({
+                    message: "Failed! Role does not exist=" + req.query.roles[i]
+                })
+                return;
+            }
+        }
+    }
+
+    next();
+}
+const verifySignup = {
+    checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
+    checkRolesExisted: checkRolesExisted
+}
+
+module.exports = verifySignup;
